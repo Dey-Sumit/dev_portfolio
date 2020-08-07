c67107897c7847511a7b65d5baf1aab4ca505312
Sumit <sumax333@gmail.com>
2020-08-08 04:27:36 +0530

    project set up and base layout created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Sidebar from './compoenets/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Uff! Finally</h1>
+    <div className="app">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="app__sidebar">
+              <Sidebar />
+            </div>
+          </div>
+          <div className="col-lg-9 app__main-content">
+            main
+      </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/compoenets/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <h1>Sidebar</h1>
+    )
+}
+
+export default Sidebar;
